9236fc3f52bbba98c05dce7263e7fd3a57c8002a
Aniruddha Chandra <aniruddha.282003@gmail.com>
2024-07-20 17:51:33 +0530

    add home.html and add style;

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-
-from flask import Flask
+from flask import Flask, render_template
 
 app: Flask = Flask(__name__, template_folder='template', static_folder='static')
+
+
+@app.route("/")
+def lode_home_page():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
